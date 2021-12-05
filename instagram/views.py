1bb391e82b9f153ee6b8da612ad5c089813941ff
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-06 00:16:37 +0300

    define serach results function

--- instagram/views.py
+++ instagram/views.py
@@ -8,3 +8,14 @@ def welcome(request):
     images = Image.objects.all()
     return render(request, 'home.html', {'images': images})
 
+def search_results(request):
+    if 'search' in request.GET and request.GET['search']:
+        search_term = request.GET.get('search').lower()
+        posts = Image.search_by_name(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {'success': message, 'posts': posts})
+    else:
+        message = 'You havent searched for any term'
+        return render(request, 'search.html', {'danger': message})
+
