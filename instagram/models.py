f1f887c0d416102dce10cb3d0e4b1725ba0f4f48
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-05 22:24:06 +0300

    add profile model

--- instagram/models.py
+++ instagram/models.py
@@ -1,51 +1,32 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-
-# Create your models here.
-class Profile(models.Model):
-    profile_photo = CloudinaryField('image')
-    bio = models.TextField()
-
-    def __str__(self):
-        return self.user.username
-
-    def update_profile(self):
-        self.save()
-
-    def save_profile(self):
-        self.save()
-
-    def delete_profile(self):
-        self.delete()
-
 class Image(models.Model):
-    image=CloudinaryField('image')
-    image_name = models.CharField(max_length=50)
-    image_caption = models.TextField()
-    posted_on = models.DateTimeField(auto_now_add=True)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    image = CloudinaryField('image', default='')
+    name = models.CharField(max_length = 30,default='')
+    caption = models.TextField()
     likes = models.IntegerField(default=0)
     comments = models.IntegerField(default=0)
 
+    
     def __str__(self):
-        return self.image_name
-
-    def delete_image(self):
-        self.delete()
-
+        return self.name
+    
+    @classmethod
+    def search_by_name(cls,search_term):
+        images = cls.objects.filter(title__icontains = search_term)
+        return images
+    
     def save_image(self):
         self.save()
 
+    def delete_image(self):
+        self.delete()
+    
     def update_caption(self, new_caption):
         self.image_caption = new_caption
         self.save()
 
-    @classmethod
-    def get_images_by_user(cls, user):
-        posts = cls.objects.filter(user=user)
-        return posts
-
-    @classmethod
-    def get_all_comments(self):
-        return self.objects.comments.all()
+class Profile(models.Model):
+    profile_photo=CloudinaryField('image')
+    bio = models.TextField(max_length=500)
