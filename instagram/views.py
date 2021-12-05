eb3e86eef6b9f17302d00496424f0785c31f729c
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-05 22:36:48 +0300

    update the views file

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 
+from instagram.models import Image
+
 # Create your views here.
 def welcome(request):
-    return render(request, 'welcome.html')
+    images = Image.objects.all()
+    return render(request, 'home.html', {'images': images})
+
