df883e15617c86e6b8efa465b736828ceaaf110c
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-05 15:47:49 +0300

    create profile and image models

--- instagram/models.py
+++ instagram/models.py
@@ -7,6 +7,9 @@ class Profile(models.Model):
     profile_photo = CloudinaryField('image')
     bio = models.TextField()
 
+    def __str__(self):
+        return self.user.username
+
     def update_profile(self):
         self.save()
 
@@ -24,3 +27,25 @@ class Image(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     likes = models.IntegerField(default=0)
     comments = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.image_name
+
+    def delete_image(self):
+        self.delete()
+
+    def save_image(self):
+        self.save()
+
+    def update_caption(self, new_caption):
+        self.image_caption = new_caption
+        self.save()
+
+    @classmethod
+    def get_images_by_user(cls, user):
+        posts = cls.objects.filter(user=user)
+        return posts
+
+    @classmethod
+    def get_all_comments(self):
+        return self.objects.comments.all()
