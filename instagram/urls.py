6ed5d7545fb4a977d92acf5accf70c762a8d97a9
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-07 22:10:20 +0300

    change images to image

--- instagram/urls.py
+++ instagram/urls.py
@@ -16,5 +16,5 @@ urlpatterns=[
     path('user/<int:id>/', views.user_profile, name='user.profile'),
     
 ]
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
